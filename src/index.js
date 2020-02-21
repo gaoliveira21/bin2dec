b62e486b8d2891b1686d270266ca4b60fef1d505
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2020-02-21 11:05:09 -0300

    interface created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Binary to Decimal converter</h1>
+      <form>
+        <div className="input-field">
+          <label htmlFor="binaryInput">Binary input</label>
+          <input type="text" name="binary" id="binaryInput" placeholder="Enter 0 or 1"/>
+        </div>
+        <input type="submit" value="Converter" />
+      </form>
+
+      <div className="decimal-output">
+        <p>Decimal output</p>
+        <div>00000</div>
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
